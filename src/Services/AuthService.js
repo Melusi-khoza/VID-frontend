e34122fac7b7454fec409436ac0a6c3bdcae147a
Melusi-khoza <melusidilu320@gmail.com>
2025-06-17 09:40:57 +0200

    AuthService.js

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://vid-backend-yyha.onrender.com/api/';
+const API_URL = 'https://18e1-168-172-120-67.ngrok-free.app/api/';
 
 
 //const API_URL2 = process.env.REACT_APP_API_URL || 'http://localhost:5265/api/';
